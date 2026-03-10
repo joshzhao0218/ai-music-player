3bfd492a8557c6158d3f2e0174b94548afa63a6c
Josh Zhao <zhaokvance@gmail.com>
2026-03-10 11:01:56 +0800

    Add main application class

---
+++ src/main/java/com/josh/music/MusicPlayerApplication.java
@@ -0,0 +1,16 @@
+package com.josh.music;
+
+import org.springframework.boot.SpringBootApplication;
+import org.springframework.boot.auto.configuration.EnableAutoConfiguration;
+
+EnableAutoConfiguration
+/**
+* AI Music Player Application
+ * Powered by Qwen LLM
+ */
+public class MusicPlayerApplication {
+    
+    public static void main(String[] args) {
+        SpringBootApplication.run(MusicPlayerApplication.class, args);
+    }
+}
